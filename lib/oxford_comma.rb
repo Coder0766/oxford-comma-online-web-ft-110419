d0ebc4c8f6a9392a09806186ab4a1556ddae4a62
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-14 00:30:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -26,7 +26,7 @@ if array.length == 2
 end
     
 if array.length == 3 
-    return array.join(", ,and ")
+    return array.join(", ,and ") + " and " + last
 end
 
 if array.length == 2 
@@ -35,4 +35,4 @@ if array.length == 2
   end
 end
 end
-
+input.join(', ') + ' and ' + last;
